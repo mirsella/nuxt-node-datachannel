5eaeac263e1b3ba9f938eb9a47aea69b0376cc72
mirsella <mirsella@protonmail.com>
2024-10-24 19:27:47 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  nitro: {
+    externals: {
+      traceInclude: [
+        "./node_modules/node-datachannel/build/Release/node_datachannel.node",
+      ],
+    },
+  },
+});
